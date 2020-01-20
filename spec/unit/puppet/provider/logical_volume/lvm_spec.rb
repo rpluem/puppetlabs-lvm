b318ca48eb521edb102f51728cf314d9b6c3f740
Ruediger Pluem <ruediger.pluem@vodafone.com>
2020-01-20 13:08:06 +0100

    Test

--- spec/unit/puppet/provider/logical_volume/lvm_spec.rb
+++ spec/unit/puppet/provider/logical_volume/lvm_spec.rb
@@ -35,6 +35,12 @@ describe provider_class do
       @provider.class.stubs(:lvs).with('data').returns(lvs_output)
       expect(@provider.exists?).to be > 10
     end
+    it "should return 'false', lv 'dat' in vg 'data' exists" do
+      @resource.expects(:[]).with(:name).returns('dat')
+      @resource.expects(:[]).with(:volume_group).returns('data').at_least_once
+      @provider.class.stubs(:lvs).with('dat').returns(lvs_output)
+      expect(@provider.exists?).to be 0
+    end
     it "should return 'nil', lv 'data' in vg 'myvg' does not exist" do
       @resource.expects(:[]).with(:volume_group).returns('myvg').at_least_once
       @provider.class.stubs(:lvs).with('myvg').raises(Puppet::ExecutionFailure, 'Execution of \'/sbin/lvs myvg\' returned 5')
